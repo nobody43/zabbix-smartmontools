f9afcfcfa32d0f0b3ee1ae1c233655854aa56f6f
nobodysu <e@mdmart.ru>
2016-09-09 01:49:54 +0300

    Working release

--- scripts/smartctl-lld.py
+++ scripts/smartctl-lld.py
@@ -1,68 +1,104 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-# path to zabbix agent configuration file
-agentConf = '/etc/zabbix/zabbix_agentd.conf'
+## Installation instructions: https://github.com/nobodysu/zabbix-smartmontools
 
-import re
+senderPyPath = r'/etc/zabbix/scripts/smartctl-send.py'               # Linux
+#senderPyPath = r'C:\zabbix-agent\scripts\smartctl-send.py'          # Win
+#senderPyPath = r'/usr/local/etc/zabbix/scripts/smartctl-send.py'    # BSD
+
+# provide raid configuration if needed
+raidOverride = []
+# like this:
+#raidOverride = ['sda -d sat+megaraid,4', 'sda -d sat+megaraid,5']
+# more info: https://www.smartmontools.org/wiki/Supported_RAID-Controllers
+
+import sys
 import subprocess
+import re
 import json
 
-senderPath = 'zabbix_sender'
 jsonData = []
 senderData = []
-isDebug = ''
 
-allDisksStdout = subprocess.getoutput('smartctl --scan')
+if not raidOverride:
+    allDisksStdout = subprocess.getoutput('smartctl --scan')
+    diskListRe = re.findall(r'^/dev/(\w+)', allDisksStdout, re.M)
+else:
+    diskListRe = raidOverride
 
-diskList = re.findall(r'^/dev/(\w+)', allDisksStdout, re.M)
-
-for d in diskList:
+for d in diskListRe:
     ctlOut = subprocess.getoutput('smartctl -a /dev/' + d)
 
+    if raidOverride:
+        d = d.replace(',', '').replace(' -d', '').replace('[', '').replace(']', '').replace('+', '_').replace(' ', '_')  # slightly sanitize the item key. for RAID only
+    #print('disk:                         ', d)
+
     modelRe = re.search(r'^Device Model:\s+(.+)$', ctlOut, re.M | re.I)
-    #print(modelRe.group(1))
+    #print(d + ': modelRe.group(1):       ' + modelRe.group(1))
     if modelRe:
         jsonData.append({'{#DMODEL}':d})
         senderData.append('- smartctl.info[' + d + ',model] "' + modelRe.group(1) + '"')
 
     serialRe = re.search(r'^Serial Number:\s+(.+)$', ctlOut, re.M | re.I)
-    #print(serialRe.group(1))
+    #print(d + ': serialRe.group(1):      ' + serialRe.group(1))
     if serialRe:
         jsonData.append({'{#DSERIAL}':d})
         senderData.append('- smartctl.info[' + d + ',serial] "' + serialRe.group(1) + '"')
 
     firmwareRe = re.search(r'^Firmware Version:\s+(.+)$', ctlOut, re.M | re.I)
-    #print(firmwareRe.group(1))
+    #print(d + ': firmwareRe.group(1):    ' + firmwareRe.group(1))
     if firmwareRe:
         jsonData.append({'{#DFIRMWARE}':d})
         senderData.append('- smartctl.info[' + d + ',firmware] "' + firmwareRe.group(1) + '"')
 
+    capacityRe = re.search(r'User Capacity:\s+(.+)bytes', ctlOut, re.I)
+    if capacityRe:
+        capacityValue = re.sub('\D', '', capacityRe.group(1))  # substitute all but numbers
+        #print(d + ': capacityValue:          ' + capacityValue)
+        jsonData.append({'{#DCAPACITY}':d})
+        senderData.append('- smartctl.info[' + d + ',capacity] "' + capacityValue + '"')
+
+    rpmRe = re.search(r'^Rotation Rate:\s+(\d+)\s+rpm$', ctlOut, re.M | re.I)
+    #print(d + ': rpmRe.group(1):         ' + rpmRe.group(1))
+    if rpmRe:
+        jsonData.append({'{#DRPM}':d})
+        senderData.append('- smartctl.info[' + d + ',rpm] "' + rpmRe.group(1) + '"')
+
     selftestRe = re.search(r'^SMART overall-health self-assessment test result:\s+(.+)$', ctlOut, re.M | re.I)
-    #print(selftestRe.group(1))
+    #print(d + ': selftestRe.group(1):    ' + selftestRe.group(1))
     if selftestRe:
         jsonData.append({'{#DSELFTEST}':d})
         senderData.append('- smartctl.info[' + d + ',selftest] "' + selftestRe.group(1) + '"')
 
     valuesRe = re.findall(r'^(?:\s+)?(\d+)\s+([a-z_-]+)\s+[\w-]+\s+\d+\s+\d+\s+\d+\s+[\w-]+\s+[\w-]+\s+[\w-]+\s+(\d+)', ctlOut, re.M | re.I)
-    #print('valuesRe:    ', valuesRe)
+    #print(d + ': valuesRe:\n', valuesRe)
+    for v in valuesRe:
+        if v[0] == '5':                         # semi-hardcoded values for triggers
+            jsonData.append({'{#DVALUE5}':d})
+        elif v[0] == '187':
+            jsonData.append({'{#DVALUE187}':d})
+        elif v[0] == '188':
+            jsonData.append({'{#DVALUE188}':d})
+        elif v[0] == '197':
+            jsonData.append({'{#DVALUE197}':d})
+        elif v[0] == '198':
+            jsonData.append({'{#DVALUE198}':d})
+        elif v[0] == '199':
+            jsonData.append({'{#DVALUE199}':d})
+        else:
+            jsonData.append({'{#DVALUE}':d, '{#SMARTID}':v[0], '{#SMARTNAME}':v[1]})  # all other possible values
 
-    for i in valuesRe:
-        jsonData.append({'{#DVALUE}':d, '{#SMARTID}':i[0], '{#SMARTNAME}':i[1]})
-        senderData.append('- smartctl.value[' + d + ',' + i[0] + '] ' + i[2])
+        senderData.append('- smartctl.value[' + d + ',' + v[0] + '] ' + v[2])
 
-#        print('DEVICE:    ', d)
-#        print('i[0]:      ', i[0])
-#        print('i[1]:      ', i[1])
+print(json.dumps({"data": jsonData}, indent=4))  # print data gathered for LLD
 
-print(json.dumps({"data": jsonData}, indent=4), flush=True)
+senderDataNStr = '\n'.join(senderData)  # items for zabbix sender separated by newlines
 
-import time
-time.sleep(2)
-
-senderDataNStr = '\n'.join(senderData)
-
-senderProc = subprocess.Popen([senderPath, isDebug, '-c', agentConf, '-i', '-'], stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, universal_newlines=True)
-senderProc.communicate(input=senderDataNStr)
-
-#for i in senderData:
-#    print(i)
+# pass senderDataNStr to smartctl-send.py:
+if sys.argv[1] == 'get':
+    subprocess.Popen([senderPyPath, 'get', senderDataNStr], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)  # spawn new process and regain shell control immediately
+elif sys.argv[1] == '-v':
+    subprocess.Popen([senderPyPath, '-v', senderDataNStr])  # do not detach if in verbose mode, also skips timeout in smartctl-send.py
+else:
+    print("smartctl-lld: Not supported. Use 'get' or '-v'.")
+    sys.exit(1)
---
+++ scripts/smartctl-send.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+## Installation instructions: https://github.com/nobodysu/zabbix-smartmontools
+
+# path to zabbix agent configuration file
+agentConf = r'/etc/zabbix/zabbix_agentd.conf'                   # Linux
+#agentConf = r'C:\zabbix_agentd.conf'                           # Win
+#agentConf = r'/usr/local/etc/zabbix24/zabbix_agentd.conf'      # BSD
+senderPath = r'zabbix_sender'                                   # Linux, BSD
+#senderPath = r'C:\zabbix-agent\bin\win32\zabbix_sender.exe'    # Win
+
+timeout = 10       # how much to wait between LLD and sending
+
+import sys
+import subprocess
+import time
+
+senderDataNStr = sys.argv[2]
+
+if sys.argv[1] == 'get':
+    time.sleep(timeout)  # wait for LLD to be processed by server
+    senderProc = subprocess.Popen([senderPath, '-c', agentConf, '-i', '-'], stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, universal_newlines=True)  # send data gathered from second argument to zabbix server
+elif sys.argv[1] == '-v':
+    print('  Data sent to zabbix sender:\n' + senderDataNStr)
+    sys.stdout.flush()  # print before proc results
+    senderProc = subprocess.Popen([senderPath, '-v', '-c', agentConf, '-i', '-'], stdin=subprocess.PIPE, universal_newlines=True)  # verbose sender output
+    print('  Note: the sender will fail if server did not gather LLD previously.')
+else:
+    print("smartctl-send: Not supported. Use 'get' or '-v'.")
+    sys.exit(1)
+
+senderProc.communicate(input=senderDataNStr)
