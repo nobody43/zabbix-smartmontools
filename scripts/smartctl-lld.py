b426d707c8de68fe09c854bda16a25dbe4cad0c1
nobodysu <e@mdmart.ru>
2016-09-26 03:47:11 +0300

    Working windows version

--- scripts/smartctl-lld.py
+++ scripts/smartctl-lld.py
@@ -2,8 +2,9 @@
 
 ## Installation instructions: https://github.com/nobodysu/zabbix-smartmontools
 
+# path to second send script
 senderPyPath = r'/etc/zabbix/scripts/smartctl-send.py'               # Linux
-#senderPyPath = r'C:\zabbix-agent\scripts\smartctl-send.py'          # Win
+#senderPyPath = 'C:\zabbix-agent\scripts\smartctl-send.py'           # Win
 #senderPyPath = r'/usr/local/etc/zabbix/scripts/smartctl-send.py'    # BSD
 
 # provide raid configuration if needed
@@ -20,17 +21,17 @@ import json
 jsonData = []
 senderData = []
 
-if not raidOverride:
-    allDisksStdout = subprocess.getoutput('smartctl --scan')
-    diskListRe = re.findall(r'^/dev/(\w+)', allDisksStdout, re.M)
+if not raidOverride:   # if RAID is not defined
+    allDisksStdout = subprocess.getoutput('smartctl --scan')   # scan the disks
+    diskListRe = re.findall(r'^/dev/(\w+)', allDisksStdout, re.M)   # and determine short device names
 else:
-    diskListRe = raidOverride
+    diskListRe = raidOverride   # or just use manually provided settings
 
-for d in diskListRe:
+for d in diskListRe:   # loop through all found drives
     ctlOut = subprocess.getoutput('smartctl -a /dev/' + d)
 
     if raidOverride:
-        d = d.replace(',', '').replace(' -d', '').replace('[', '').replace(']', '').replace('+', '_').replace(' ', '_')  # slightly sanitize the item key. for RAID only
+        d = d.replace(',', '').replace(' -d', '').replace('[', '').replace(']', '').replace('+', '_').replace('  ', ' ').replace(' ', '_')  # slightly sanitize the item key. for RAID only
     #print('disk:                         ', d)
 
     modelRe = re.search(r'^Device Model:\s+(.+)$', ctlOut, re.M | re.I)
@@ -53,7 +54,7 @@ for d in diskListRe:
 
     capacityRe = re.search(r'User Capacity:\s+(.+)bytes', ctlOut, re.I)
     if capacityRe:
-        capacityValue = re.sub('\D', '', capacityRe.group(1))  # substitute all but numbers
+        capacityValue = re.sub('\D', '', capacityRe.group(1))   # substitute all but numbers
         #print(d + ': capacityValue:          ' + capacityValue)
         jsonData.append({'{#DCAPACITY}':d})
         senderData.append('- smartctl.info[' + d + ',capacity] "' + capacityValue + '"')
@@ -86,19 +87,26 @@ for d in diskListRe:
         elif v[0] == '199':
             jsonData.append({'{#DVALUE199}':d})
         else:
-            jsonData.append({'{#DVALUE}':d, '{#SMARTID}':v[0], '{#SMARTNAME}':v[1]})  # all other possible values
+            jsonData.append({'{#DVALUE}':d, '{#SMARTID}':v[0], '{#SMARTNAME}':v[1]})    # all other possible values
 
         senderData.append('- smartctl.value[' + d + ',' + v[0] + '] ' + v[2])
 
-print(json.dumps({"data": jsonData}, indent=4))  # print data gathered for LLD
+print(json.dumps({"data": jsonData}, indent=4))   # print data gathered for LLD
 
-senderDataNStr = '\n'.join(senderData)  # items for zabbix sender separated by newlines
+senderDataNStr = '\n'.join(senderData)   # items for zabbix sender separated by newlines
 
 # pass senderDataNStr to smartctl-send.py:
-if sys.argv[1] == 'get':
-    subprocess.Popen([senderPyPath, 'get', senderDataNStr], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)  # spawn new process and regain shell control immediately
-elif sys.argv[1] == '-v':
-    subprocess.Popen([senderPyPath, '-v', senderDataNStr])  # do not detach if in verbose mode, also skips timeout in smartctl-send.py
-else:
+if sys.platform != 'win32':   # if not windows
+    if sys.argv[1] == 'get':
+        subprocess.Popen([senderPyPath, 'get', senderDataNStr], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)    # spawn new process and regain shell control immediately
+    elif sys.argv[1] == '-v':
+        subprocess.Popen([senderPyPath, '-v', senderDataNStr])   # do not detach if in verbose mode, also skips timeout in smartctl-send.py
+else:   # windows have certain caveats
+    if sys.argv[1] == 'get':
+        subprocess.Popen(['python', senderPyPath, 'get', senderDataNStr], stdout=None, stderr=None, stdin=None)    
+    elif sys.argv[1] == '-v':
+        subprocess.Popen(['python', senderPyPath, '-v', senderDataNStr])
+
+if sys.argv[1] != 'get' and sys.argv[1] != '-v':
     print("smartctl-lld: Not supported. Use 'get' or '-v'.")
     sys.exit(1)
