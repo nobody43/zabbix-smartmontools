86f3bd57ad0f2479bb259cdf030c905219327dde
nobodysu <e@mdmart.ru>
2017-04-21 21:11:07 +0300

    Now utilises smartctl error return codes, Proper exception based on process error codes, Reverted to communication through arguments, Some bugs were fixed

--- scripts/smartctl-lld.py
+++ scripts/smartctl-lld.py
@@ -4,18 +4,29 @@
 
 mode = 'device'   # 'device' or 'serial' as primary identifier in zabbix item's name
 
-ctlPath = 'smartctl'
-#ctlPath = r'"C:\Program Files\smartmontools\bin\smartctl.exe"'   # if smartctl isn't in PATH
+ctlPath = r'smartctl'
+#ctlPath = r'C:\Program Files\smartmontools\bin\smartctl.exe'   # if smartctl isn't in PATH
+#ctlPath = r'/usr/local/sbin/smartctl'
 
 # path to second send script
-senderPyPath = r'/etc/zabbix/scripts/smartctl-send.py'               # Linux
-#senderPyPath = r'C:\zabbix-agent\scripts\smartctl-send.py'          # Win
-#senderPyPath = r'/usr/local/etc/zabbix/scripts/smartctl-send.py'    # BSD
+senderPyPath = r'/etc/zabbix/scripts/smartctl-send.py'              # Linux
+#senderPyPath = r'C:\zabbix-agent\scripts\smartctl-send.py'         # Win
+#senderPyPath = r'/usr/local/etc/zabbix/scripts/smartctl-send.py'   # BSD
+
+# path to zabbix agent configuration file
+agentConf = r'/etc/zabbix/zabbix_agentd.conf'                       # Linux
+#agentConf = r'C:\zabbix_agentd.conf'                               # Win
+#agentConf = r'/usr/local/etc/zabbix24/zabbix_agentd.conf'          # BSD
+
+senderPath = r'zabbix_sender'                                       # Linux, BSD
+#senderPath = r'C:\zabbix-agent\bin\win32\zabbix_sender.exe'        # Win
+
+timeout = '60'   # how long the script must wait between LLD and sending, increase if data received late (does not affect windows)
 
 # manually provide disk list or RAID configuration if needed
 diskListManual = []
 # like this:
-#diskListManual = ['sda -d sat+megaraid,4', 'sda -d sat+megaraid,5']
+#diskListManual = ['/dev/sda -d sat+megaraid,4', '/dev/sda -d sat+megaraid,5']
 # more info: https://www.smartmontools.org/wiki/Supported_RAID-Controllers
 
 ## End of configuration ##
@@ -23,38 +34,86 @@ diskListManual = []
 import sys
 import subprocess
 import re
-import json
+from json import dumps
+from shlex import split
 
 hostname = '"' + sys.argv[2] + '"'
 
 jsonData = []
 senderData = []
-allDisksStdout = ''
-ctlOut = ''
 
-stopChars = [(' -d', ''), (',', ''), ('[', ' '), (']', ' '), ('+', '_'), ('  ', ' '), ('/', '_'), ('~', '_'), ('`', '_'), ('@', '_'), ('#', '_'), ('$', '_'), ('%', '_'), ('^', '_'), ('&', '_'), ('*', '_'), ('(', '_'), (')', '_'), ('{', '_'), ('}', '_'), ('=', '_'), (':', '_'), (';', '_'), ('"', '_'), ('?', '_'), ('<', '_'), ('>', '_'), (' ', '_')]
+stopChars = [('/dev/', ''), (' -d', ''), (',', ''), ('[', ' '), (']', ' '), ('+', '_'), ('  ', ' '), ('/', '_'), ('~', '_'), ('`', '_'), ('@', '_'), ('#', '_'), ('$', '_'), ('%', '_'), ('^', '_'), ('&', '_'), ('*', '_'), ('(', '_'), (')', '_'), ('{', '_'), ('}', '_'), ('=', '_'), (':', '_'), (';', '_'), ('"', '_'), ('?', '_'), ('<', '_'), ('>', '_'), (' ', '_')]
 
 def replace_all(string, stopChars):
     for i, j in stopChars:
         string = string.replace(i, j)
     return string
 
+allDisksStdout = ''
+configStatusError = ''
 if not diskListManual:   # if manual list is not provided
-    allDisksStdout = subprocess.getoutput(ctlPath + ' --scan')   # scan the disks
-    diskListRe = re.findall(r'^/dev/([^ ]+)', allDisksStdout, re.M)   # and determine short device names
+    try:
+        allDisksStdout = subprocess.check_output([ctlPath, '--scan'], universal_newlines=True)   # scan the disks
+    except OSError as e:
+        if e.args[0] == 2:
+            configStatusError = 'SCAN_OS_NOCMD'
+        else:
+            configStatusError = 'SCAN_OS_ERROR'
+    except:
+        configStatusError = 'SCAN_UNKNOWN_ERROR'
+
+        if sys.argv[1] == 'getverb':
+            raise
+
+    diskListRe = re.findall(r'^(/dev/[^ ]+)', allDisksStdout, re.M)   # and determine short device names
 else:
     diskListRe = diskListManual   # or just use manually provided settings
 #print(diskListRe)
 
-for d in diskListRe:   # loop through all found drives
-    ctlOut = subprocess.getoutput(ctlPath + ' -a /dev/' + d)
 
+for d in diskListRe:   # loop through all found drives
+    deviceNameOrig = d
     d = replace_all(d, stopChars)   # sanitize the item key
+    deviceName = d   # save device name before mode selection and after manual substitution
     #print('disk:                         ', d)
 
-    deviceName = d   # save device name before mode selection and after manual substitution
+    try:
+        diskProc = subprocess.check_output([ctlPath, '-a'] + split(deviceNameOrig), universal_newlines=True)   # take string from 'diskListRe', make arguments from it and append to existing command, then run it
+    except OSError as e:
+        if e.args[0] == 2:
+            configStatusError = 'D_OS_NOCMD'
+            break
+        else:
+            configStatusError = 'D_OS_ERROR'
+            break
+    except subprocess.CalledProcessError as e:   # handle process-specific errors
+        ''' see 'man smartctl' for more information
+        Bit 0 = Code 1
+        Bit 1 = Code 2
+        Bit 2 = Code 4
+        Bit 3 = Code 8
+        Bit 4 = Code 16
+        Bit 5 = Code 32
+        Bit 6 = Code 64
+        Bit 7 = Code 128
+        '''
+        diskProc = e.output   # substitude output even on error, so it can be processed further
+        senderData.append(hostname + ' smartctl.info[' + d + ',DriveStatus] "ERR_CODE_' + str(e.args[0]) + '"')
 
-    serialRe = re.search(r'^Serial Number:\s+(.+)$', ctlOut, re.M | re.I)
+        if e.args[0] == 1 or e.args[0] == 2:
+            continue   # continue to the next disk on fatal error
+    except:
+        diskProc = e.output
+        senderData.append(hostname + ' smartctl.info[' + d + ',DriveStatus] "UNKNOWN_ERROR_ON_PROCESSING"')
+
+        if sys.argv[1] == 'getverb':
+            raise
+    else:
+        senderData.append(hostname + ' smartctl.info[' + d + ',DriveStatus] "PROCESSED"')   # no trigger assigned, but its needed as a fallback value
+    finally:
+        jsonData.append({'{#DDRIVESTATUS}':d})   # it will fire even on OSError (no senderData)
+
+    serialRe = re.search(r'^Serial Number:\s+(.+)$', diskProc, re.M | re.I)
     #print(d + ': serialRe.group(1):      ' + serialRe.group(1))
     if serialRe:
         if mode == 'serial':
@@ -67,44 +126,43 @@ for d in diskListRe:   # loop through all found drives
     jsonData.append({'{#DNAME}':d})
     senderData.append(hostname + ' smartctl.info[' + d + ',device] "' + deviceName + '"')
 
-    modelRe = re.search(r'^Device Model:\s+(.+)$', ctlOut, re.M | re.I)
+    modelRe = re.search(r'^Device Model:\s+(.+)$', diskProc, re.M | re.I)
     #print(d + ': modelRe.group(1):       ' + modelRe.group(1))
     if modelRe:
         jsonData.append({'{#DMODEL}':d})
         senderData.append(hostname + ' smartctl.info[' + d + ',model] "' + modelRe.group(1) + '"')
 
-    firmwareRe = re.search(r'^Firmware Version:\s+(.+)$', ctlOut, re.M | re.I)
+    firmwareRe = re.search(r'^Firmware Version:\s+(.+)$', diskProc, re.M | re.I)
     #print(d + ': firmwareRe.group(1):    ' + firmwareRe.group(1))
     if firmwareRe:
         jsonData.append({'{#DFIRMWARE}':d})
         senderData.append(hostname + ' smartctl.info[' + d + ',firmware] "' + firmwareRe.group(1) + '"')
 
-    capacityRe = re.search(r'User Capacity:\s+(.+)bytes', ctlOut, re.I)
+    capacityRe = re.search(r'User Capacity:\s+(.+)bytes', diskProc, re.I)
     if capacityRe:
         capacityValue = re.sub('\D', '', capacityRe.group(1))   # substitute all but numbers
         #print(d + ': capacityValue:          ' + capacityValue)
         jsonData.append({'{#DCAPACITY}':d})
         senderData.append(hostname + ' smartctl.info[' + d + ',capacity] "' + capacityValue + '"')
 
-    rpmRe = re.search(r'^Rotation Rate:\s+(\d+)\s+rpm$', ctlOut, re.M | re.I)
+    rpmRe = re.search(r'^Rotation Rate:\s+(\d+)\s+rpm$', diskProc, re.M | re.I)
     #print(d + ': rpmRe.group(1):         ' + rpmRe.group(1))
     if rpmRe:
         jsonData.append({'{#DRPM}':d})
         senderData.append(hostname + ' smartctl.info[' + d + ',rpm] "' + rpmRe.group(1) + '"')
 
-    selftestRe = re.search(r'^SMART overall-health self-assessment test result:\s+(.+)$', ctlOut, re.M | re.I)
+    selftestRe = re.search(r'^SMART overall-health self-assessment test result:\s+(.+)$', diskProc, re.M | re.I)
     #print(d + ': selftestRe.group(1):    ' + selftestRe.group(1))
     if selftestRe:
         jsonData.append({'{#DSELFTEST}':d})
         senderData.append(hostname + ' smartctl.info[' + d + ',selftest] "' + selftestRe.group(1) + '"')
 
-    valuesRe = re.findall(r'^(?:\s+)?(\d+)\s+([\w-]+)\s+[\w-]+\s+\d{3}\s+\d{3}\s+\d{3}\s+[\w-]+\s+[\w-]+\s+[\w-]+\s+(\d+)', ctlOut, re.M | re.I)   # catch id, name and value
+    valuesRe = re.findall(r'^(?:\s+)?(\d+)\s+([\w-]+)\s+[\w-]+\s+\d{3}\s+\d{3}\s+\d{3}\s+[\w-]+\s+[\w-]+\s+[\w-]+\s+(\d+)', diskProc, re.M | re.I)   # catch id, name and value
     #print(d + ': valuesRe:\n', valuesRe)
 
-
+    jsonData.append({'{#DSMARTSTATUS}':d})
     if valuesRe:
-        jsonData.append({'{#DSMARTSTATUS}':d})
-        senderData.append(hostname + ' smartctl.info[' + d + ',SmartStatus] "PRESENT"')
+        senderData.append(hostname + ' smartctl.info[' + d + ',SmartStatus] "PRESENT"')   # item does not correctly represents action, will be changed in future
 
         for v in valuesRe:
             if v[0] == '5':                         # semi-hardcoded values for triggers
@@ -125,45 +183,32 @@ for d in diskListRe:   # loop through all found drives
             senderData.append(hostname + ' smartctl.value[' + d + ',' + v[0] + '] ' + v[2])
 
     else:
-        jsonData.append({'{#DSMARTSTATUS}':d})
         senderData.append(hostname + ' smartctl.info[' + d + ',SmartStatus] "NO_SMART_VALUES"')
 
-
-if senderData:
-    senderData.append(hostname + ' smartctl.info[ConfigStatus] "CONFIGURED"')   # signals that client host is configured
+if configStatusError != '':
+    senderData.append(hostname + ' smartctl.info[ConfigStatus] "' + configStatusError + '"')
+elif not diskListRe:
+    senderData.append(hostname + ' smartctl.info[ConfigStatus] "NODISKS"')   # if no disks were found
 else:
-    if ctlOut.find('ermission denied') != -1 or ctlOut.find('missing admin rights') != -1:
-        senderData.append(hostname + ' smartctl.info[ConfigStatus] "MISSINGRIGHTS"')
-    elif allDisksStdout.find('smartctl: not found') != -1 or allDisksStdout.find('\"smartctl\" ') != -1:
-        senderData.append(hostname + ' smartctl.info[ConfigStatus] "NOCMD"')
-    elif not diskListRe:
-        senderData.append(hostname + ' smartctl.info[ConfigStatus] "NODISKS"')   # if no disks were found
-    else:
-        senderData.append(hostname + ' smartctl.info[ConfigStatus] "ERROR"')   # something went wrong
+    senderData.append(hostname + ' smartctl.info[ConfigStatus] "CONFIGURED"')   # signals that client host is configured
 
-senderDataNStr = '\n'.join(senderData)   # items for zabbix sender separated by system-specific newlines
+senderDataNStr = '\n'.join(senderData)   # items for zabbix sender separated by newlines
 
 if sys.platform != 'win32':   # if not windows
-    cmd = 'python3'
+    pythonCmd = 'python3'
 else:
-    cmd = 'python.exe'
+    pythonCmd = 'python.exe'
 
 # pass senderDataNStr to smartctl-send.py:
 if sys.argv[1] == 'get':
-    print(json.dumps({"data": jsonData}, indent=4))   # print data gathered for LLD
+    print(dumps({"data": jsonData}, indent=4))   # print data gathered for LLD
 
-    senderPy = subprocess.Popen([cmd, senderPyPath, 'get'], stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, universal_newlines=True)   # spawn new process and regain shell control immediately
-    try:
-        senderPy.communicate(input=senderDataNStr, timeout=1)
-    except:
-        pass
+    subprocess.Popen([pythonCmd, senderPyPath, 'get', agentConf, senderPath, timeout, senderDataNStr], stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)   # spawn new process and regain shell control immediately (only *nix, windows waits)
+
 elif sys.argv[1] == 'getverb':
-    senderPy = subprocess.Popen([cmd, senderPyPath, 'getverb'], stdin=subprocess.PIPE, universal_newlines=True)   # do not detach if in verbose mode, also skips timeout in smartctl-send.py
-    try:
-        senderPy.communicate(input=senderDataNStr)
-    except:
-        pass
+    subprocess.Popen([pythonCmd, senderPyPath, 'getverb', agentConf, senderPath, timeout, senderDataNStr], stdin=subprocess.PIPE)   # do not detach if in verbose mode, also skips timeout in smartctl-send.py
+
 else:
     print(sys.argv[0] + " : Not supported. Use 'get' or 'getverb'.")
-    sys.exit(1)
+sys.exit(1)
 
--- scripts/smartctl-send.py
+++ scripts/smartctl-send.py
@@ -1,40 +1,30 @@
 #!/usr/bin/env python3
 
-## Installation instructions: https://github.com/nobodysu/zabbix-smartmontools
-
-# path to zabbix agent configuration file
-agentConf = r'/etc/zabbix/zabbix_agentd.conf'                   # Linux
-#agentConf = r'C:\zabbix_agentd.conf'                           # Win
-#agentConf = r'/usr/local/etc/zabbix24/zabbix_agentd.conf'      # BSD
-
-senderPath = 'zabbix_sender'                                    # Linux, BSD
-#senderPath = r'C:\zabbix-agent\bin\win32\zabbix_sender.exe'    # Win
-
-timeout = 60   # how long the script must wait between LLD and sending, increase if data received late (does not affect windows)
-
-## End of configuration ##
+## Configuration was moved to 'smartctl-lld.py'
 
 import sys
-import subprocess
+from subprocess import Popen, PIPE
 from time import sleep
-import re
 
-stdin4Sender = sys.stdin.read()
+agentConf = sys.argv[2]
+senderPath = sys.argv[3]
+timeout = int(sys.argv[4])
+senderDataNStr = sys.argv[5]
 
 if sys.platform == 'win32':   # if windows
     timeout = 0   # probably permanent workaround
 
 if sys.argv[1] == 'get':
     sleep(timeout)   # wait for LLD to be processed by server
-    senderProc = subprocess.Popen([senderPath, '-c', agentConf, '-i', '-'],  stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, universal_newlines=True)   # send data gathered from second argument to zabbix server
+    senderProc = Popen([senderPath, '-c', agentConf, '-i', '-'], stdin=PIPE, universal_newlines=True)   # send data gathered from second argument to zabbix server
 elif sys.argv[1] == 'getverb':
     print('\n  Note: the sender will fail if server did not gather LLD previously.')
     print('\n  Data sent to zabbix sender:\n')
-    print(stdin4Sender)
-    senderProc = subprocess.Popen([senderPath, '-vv', '-c', agentConf, '-i', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)   # verbose sender output
+    print(senderDataNStr)
+    senderProc = Popen([senderPath, '-vv', '-c', agentConf, '-i', '-'], stdin=PIPE, universal_newlines=True)   # verbose sender output
 else:
     print(sys.argv[0] + " : Not supported. Use 'get' or 'getverb'.")
     sys.exit(1)
 
-senderProc.communicate(input=stdin4Sender)
+senderProc.communicate(input=senderDataNStr)
 
